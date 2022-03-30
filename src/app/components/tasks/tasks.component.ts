c059ff84d39340f080067dd1c4d8b1acc50814a3
Collo <mutaic84@gmail.com>
2022-03-30 13:01:55 +0300

    set up toggleReminder method

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -29,4 +29,10 @@ export class TasksComponent implements OnInit {
     ))
   }
 
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    // console.log(task.reminder)
+    this.taskService.updateTaskReminder(task).subscribe()
+  }
+
 }
